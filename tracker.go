5611ceac576e7795c1e2e7608dce39f814893ea6
steve <stevevan@hoiio.com>
2015-06-29 23:39:34 +0700

    added sample

--- tracker.go
+++ tracker.go
@@ -2,13 +2,23 @@ package tracker
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/trigger", handleTrigger)
 }
 
 func handler(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(rw, "Hello, world")
+}
+
+func handleTrigger(rw http.ResponseWriter, req *http.Request) {
+	resp, _ := http.Get("http://steamcommunity.com/market/listings/570/Scorching%20Talon")
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
 }
