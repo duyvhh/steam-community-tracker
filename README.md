399ea8891c605f9c1eb168e24c5ff42338c583d9
steve <vanhuynhduy@gmail.com>
2015-06-30 17:37:06 +0700

    completed with updated README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # steam-community-tracker
 To track a certain item's price in Steam Community market
+
+## Requirements
+
+As this project is based on Google App Engine (GAE), you must have a Google Developer account to be able to run. 
+
+## Usage
+
+1. Clone the repository
+2. Modify the URL in tracker.go to point to your favorite item on Steam community market you wish to track its price
+3. Modify the SENDER const with your registered developer's email address
+4. Modify the receiver's address at line 76
+5. Modify your threshold, below which an email notification will be sent to notify you
+
+Follow this guide to upload your application:
+https://cloud.google.com/appengine/docs/go/gettingstarted/uploading
+
+By default, the tracker will run every 15 minutes. You can modify this value in cron.yaml file, following Google Cron Format
+
+https://cloud.google.com/appengine/docs/python/config/cron#Python_app_yaml_The_schedule_format
+
+## Known limitations
+
+1. Only USD currency is supported
+2. Can only track 1 item at the moment. A workaround is to deploy multiple GAE applications.
+
+## Credits
+This project is using the following library to extract price information from steam community market
+
+- GoQuery (github.com/PuerkitoBio/goquery)
--- tracker.go
+++ tracker.go
@@ -1,24 +1,84 @@
 package tracker
 
 import (
+	"appengine"
+	"appengine/mail"
+	"appengine/urlfetch"
 	"fmt"
-	"io/ioutil"
+	"github.com/PuerkitoBio/goquery"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
+const ITEM_URL string = "<ITEM_URL_HERE>"
+const SENDER string = "<SENDER_EMAIL_ADDRESS>"
+const THRESHOLD float64 = 15.00 //THRESHOLD
+const MESSAGE string = `
+Your favorite item has its price dropped below your threshold!
+
+Item URL: %s
+Threshold Set: %f
+Current Price: %f
+
+Good luck!
+`
+
 func init() {
-	http.HandleFunc("/", handler)
 	http.HandleFunc("/trigger", handleTrigger)
 }
 
-func handler(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(rw, "Hello, world")
-}
-
 func handleTrigger(rw http.ResponseWriter, req *http.Request) {
-	resp, _ := http.Get("http://steamcommunity.com/market/listings/570/Scorching%20Talon")
+	c := appengine.NewContext(req)
+	client := urlfetch.Client(c)
+
+	resp, err := client.Get(ITEM_URL)
+
+	if err != nil {
+		c.Infof(err.Error())
+		return
+	}
 
 	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	doc, err := goquery.NewDocumentFromResponse(resp)
+
+	if err != nil {
+		c.Infof(err.Error())
+		return
+	}
+
+	var price float64 = 0.0
+
+	doc.Find("span.market_listing_price_with_fee").Each(func(i int, s *goquery.Selection) {
+
+		priceVal := strings.TrimSpace(s.Text())
+
+		if price != 0 {
+			return
+		}
+
+		if strings.HasSuffix(priceVal, "USD") || strings.HasPrefix(priceVal, "$") {
+			price, _ = strconv.ParseFloat(strings.TrimPrefix(strings.TrimSuffix(priceVal, " USD"), "$"), 64)
+		}
+	})
+
+	if price < THRESHOLD {
+		sendEmail(price, c)
+	}
+
+	fmt.Fprintf(rw, "Triggered: %f", price)
+}
+
+func sendEmail(price float64, c appengine.Context) {
+	msg := &mail.Message{
+		Sender:  SENDER,
+		To:      []string{"<RECIPIENT_EMAIL_ADDRESSES"}, //comma-delimited email addresses
+		Subject: "Steam Item price dropped below threshold",
+		Body:    fmt.Sprintf(MESSAGE, ITEM_URL, THRESHOLD, price),
+	}
+
+	if err := mail.Send(c, msg); err != nil {
+		c.Errorf("Couldn't send email: %v", err)
+	}
 }
